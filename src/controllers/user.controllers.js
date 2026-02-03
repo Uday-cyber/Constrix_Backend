e51a3ee2d7edb840ce3403f4e66ba81f281b4d8d
Your Name <you@example.com>
2026-02-03 06:04:43 +0530

    feat: implement user authentication and validation middleware

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,17 @@
+import User from "../models/user.model.js";
+
+const generateTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+
+    const accessToken = await user.generateAccessToken();
+    const refreshToken = await user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, {}, "Unable to generate tokens");
+  }
+};
